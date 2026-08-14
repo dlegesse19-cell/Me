6d74b1a8b868fb516e0260ba30816f4000744396
dlegesse19-cell <dlegesse19@gmail.com>
2026-08-14 13:37:32 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -22,6 +22,6 @@ UserSchema.pre('save', async function(next) {
 
 UserSchema.methods.comparePassword = async function(password) {
   return await bcrypt.compare(password, this.password);
-});
+};
 
 module.exports = mongoose.model('User', UserSchema);
